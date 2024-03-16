d93dd480ba7fe0a11699ce32015867a3be77b14a
MohammedRifadCyberSuare <rifad@cybersquare.com>
2024-03-16 13:48:52 +0530

    old project for reference

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  // This tells ESLint to load the config from the package `eslint-config-custom`
+  ignorePatterns: ['*/'],
+  // extends: ["custom"],
+  // settings: {
+  //   next: {
+  //     rootDir: ["web/", "space/"],
+  //   },
+  // },
+};
